5644d3b03a3e539eda8e6b036ee3de6f2e8dc1b5
Nanidni Nayak <nandini.nayak@coderacademy.edu.au>
2018-02-01 16:17:03 +1100

    fork side branch added

---
+++ app/controllers/fork_controller.rb
@@ -0,0 +1,4 @@
+class ForkController < ApplicationController
+  def page
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'fork/page'
+
   get 'item/page'
 
   resources :equipment
